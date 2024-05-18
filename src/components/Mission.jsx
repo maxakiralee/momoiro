1ab1e8d2a89caf12cb62a3c134cecc2ba9f063c3
Erin Wu <erinwu0825@ucla.edu>
2024-05-18 15:32:08 -0700

    new stuff from Erin

--- src/components/Mission.jsx
+++ src/components/Mission.jsx
@@ -1,28 +1,48 @@
 import React from 'react';
-import './Mission.css'; // Ensure to import the CSS file
+import { LifebuoyIcon, NewspaperIcon, PhoneIcon } from '@heroicons/react/20/solid';
 
-function Mission() {
+const cards = [
+  {
+    name: 'Sales',
+    description: 'Consectetur vel non. Rerum ut consequatur nobis unde. Enim est quo corrupti consequatur.',
+    icon: PhoneIcon,
+  },
+  {
+    name: 'Technical Support',
+    description: 'Quod possimus sit modi rerum exercitationem quaerat atque tenetur ullam.',
+    icon: LifebuoyIcon,
+  },
+  {
+    name: 'Media Inquiries',
+    description: 'Ratione et porro eligendi est sed ratione rerum itaque. Placeat accusantium impedit eum odit.',
+    icon: NewspaperIcon,
+  },
+];
+
+export default function Mission() {
   return (
-    <div className="mission-container">
-      <div className="card">
-        <h2>Title One</h2>
-        <p>This is some text about the first mission.</p>
+    <div className="relative isolate overflow-hidden py-24 sm:py-32">
+     
+      <div className="mx-auto max-w-7xl px-6 lg:px-8">
+        <div className="mx-auto max-w-2xl lg:mx-0">
+          <h2 className="text-4xl font-bold tracking-tight text-black sm:text-6xl">About Maui Strong</h2>
+          <p className="mt-6 text-lg leading-8 text-black-300">
+            Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet
+            fugiat veniam occaecat fugiat aliqua.
+          </p>
+        </div>
+        <div className="mx-auto mt-16 grid max-w-2xl grid-cols-1 gap-6 sm:mt-20 lg:mx-0 lg:max-w-none lg:grid-cols-3 lg:gap-8">
+          {cards.map((card) => (
+            <div key={card.name} className="flex gap-x-4 rounded-xl bg-black/5 p-6 ring-1 ring-inset ring-black/10">
+              <card.icon className="h-7 w-5 flex-none text-indigo-400" aria-hidden="true" />
+              <div className="text-base leading-7">
+                <h3 className="font-semibold text-black">{card.name}</h3>
+                <p className="mt-2 text-black-300">{card.description}</p>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
-      <div className="card">
-        <h2>Title Two</h2>
-        <p>This is some text about the second mission.</p>
-      </div>
-      <div className="card">
-        <h2>Title Three</h2>
-        <p>This is some text about the third mission.</p>
-      </div>
-      <div className="card">
-        <h2>Title Four</h2>
-        <p>This is some text about the fourth mission.</p>
-      </div>
-      <h1>Mission Statement </h1>
     </div>
   );
 }
-
-export default Mission;
--- src/components/pages/Events.jsx
+++ src/components/pages/Events.jsx
@@ -1,89 +1,87 @@
 import React from 'react';
+import Drop from '../Drop';
 
-const people = [
+
+const events = [
   {
-    name: 'Leslie Alexander',
-    email: 'leslie.alexander@example.com',
+    name: 'Mindful Mornings: Yoga & Meditation at Sunrise',
+    date: 'May 1',
+    time: '5am',
     role: 'Co-Founder / CEO',
     imageUrl:
-      'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+    'https://images.unsplash.com/photo-1506126613408-eca07ce68773?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8eW9nYXxlbnwwfHwwfHx8MA%3D%3D',
     lastSeen: '3h ago',
     lastSeenDateTime: '2023-01-23T13:23Z',
   },
   {
-    name: 'Michael Foster',
-    email: 'michael.foster@example.com',
+    name: 'Art Heals: A Creative Escape Workshop',
+    date: 'May 13',
+    time: '3pm',
     role: 'Co-Founder / CTO',
     imageUrl:
-      'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+      'https://images.unsplash.com/photo-1506126613408-eca07ce68773?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8eW9nYXxlbnwwfHwwfHx8MA%3D%3D',
     lastSeen: '3h ago',
     lastSeenDateTime: '2023-01-23T13:23Z',
   },
   {
-    name: 'Dries Vincent',
-    email: 'dries.vincent@example.com',
-    role: 'Business Relations',
+    name: 'Echoes of Maui: Storytelling Under the Stars',
+    date: 'May 13',
+    time: '3pm',
+        role: 'Business Relations',
     imageUrl:
-      'https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+    'https://images.unsplash.com/photo-1506126613408-eca07ce68773?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8eW9nYXxlbnwwfHwwfHx8MA%3D%3D',
     lastSeen: null,
   },
   {
-    name: 'Lindsay Walton',
-    email: 'lindsay.walton@example.com',
+    name: 'Family Fun Day: Games and Wellness Activities',
+    date: 'May 13',
+    time: '3pm',
     role: 'Front-end Developer',
     imageUrl:
-      'https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+    'https://images.unsplash.com/photo-1506126613408-eca07ce68773?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8eW9nYXxlbnwwfHwwfHx8MA%3D%3D',
     lastSeen: '3h ago',
     lastSeenDateTime: '2023-01-23T13:23Z',
   },
   {
-    name: 'Courtney Henry',
-    email: 'courtney.henry@example.com',
-    role: 'Designer',
+    name: 'Maui Movie Nights: Films for Thought and Healing',
+    date: 'May 13',
+    time: '3pm',
+   role: 'Designer',
     imageUrl:
-      'https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+    'https://images.unsplash.com/photo-1506126613408-eca07ce68773?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8eW9nYXxlbnwwfHwwfHx8MA%3D%3D',
     lastSeen: '3h ago',
     lastSeenDateTime: '2023-01-23T13:23Z',
   },
-  {
-    name: 'Tom Cook',
-    email: 'tom.cook@example.com',
-    role: 'Director of Product',
-    imageUrl:
-      'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-    lastSeen: null,
-  },
+  
 ]
 
 export default function Events() {
   return (
-    <ul role="list" className="divide-y divide-gray-100">
-      {people.map((person) => (
-        <li key={person.email} className="flex justify-between gap-x-6 py-5">
-          <div className="flex min-w-0 gap-x-4">
-            <img className="h-12 w-12 flex-none rounded-full bg-gray-50" src={person.imageUrl} alt="" />
-            <div className="min-w-0 flex-auto">
-              <p className="text-sm font-semibold leading-6 text-gray-900">{person.name}</p>
-              <p className="mt-1 truncate text-xs leading-5 text-gray-500">{person.email}</p>
-            </div>
-          </div>
-          <div className="hidden shrink-0 sm:flex sm:flex-col sm:items-end">
-            <p className="text-sm leading-6 text-gray-900">{person.role}</p>
-            {person.lastSeen ? (
-              <p className="mt-1 text-xs leading-5 text-gray-500">
-                Last seen <time dateTime={person.lastSeenDateTime}>{person.lastSeen}</time>
-              </p>
-            ) : (
-              <div className="mt-1 flex items-center gap-x-1.5">
-                <div className="flex-none rounded-full bg-emerald-500/20 p-1">
-                  <div className="h-1.5 w-1.5 rounded-full bg-emerald-500" />
-                </div>
-                <p className="text-xs leading-5 text-gray-500">Online</p>
+    <div>
+      <div className="flex justify-between items-center"> {/* Flex container to align items horizontally */}
+        <h1 className="p-10" >Sign up for new events today! </h1>
+        <div className="p-10">
+  <Drop />
+</div>
+
+      </div>
+      <ul role="list" className="divide-y divide-gray-200 p-10">
+        {events.map((person) => (
+          <li key={person.email} className="flex justify-between gap-x-6 py-5">
+            <div className="flex min-w-0 gap-x-4">
+              <img className="h-20 w-20 flex-none rounded-full bg-gray-50" src={person.imageUrl} alt="" />
+              <div className="min-w-0 flex-auto">
+                <p className="text-4xl font-semibold leading-6 text-gray-900">{person.name}</p>
+                <p className="mt-1 truncate text-lg leading-5 text-gray-500">{person.date}, 2024 {person.time} </p>
               </div>
-            )}
-          </div>
-        </li>
-      ))}
-    </ul>
-  )
+            </div>
+            <div className="hidden shrink-0 sm:flex sm:flex-col sm:items-end">
+              <p className="text-xl leading-6 text-gray-900">{person.role}</p>
+      
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
