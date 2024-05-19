b6428f18dde25fabc718950e6907eec8f4b016ac
Rohan Jadhav <itsrohanj@gmail.com>
2024-05-19 01:35:48 -0700

    homepage finalizations

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -6,25 +6,27 @@ import '../styling/HeroSection.css';
 function HeroSection() {
   return (
     <div className='hero-container'>
-      
- 
-      <p>Empowering our community to thrive through support, healing, and connection</p>
-      <div className='hero-btns'>
-        <Button
-          className='btns'
-          buttonStyle='btn--outline'
-          buttonSize='btn--large'
-        >
-          GET STARTED
-        </Button>
-        <Button
-          className='btns'
-          buttonStyle='btn--primary'
-          buttonSize='btn--large'
-          onClick={console.log('hey')}
-        >
-          WATCH TRAILER <i className='far fa-play-circle' />
-        </Button>
+      <div className='hero-image'></div>
+      <div className='hero-content'>
+        <h1>Maui Strong</h1>
+        <p>Empowering our community to thrive through support, healing, and connection</p>
+        <div className='hero-btns'>
+          <Button
+            className='btns'
+            buttonStyle='btn--outline'
+            buttonSize='btn--large'
+          >
+            GET STARTED
+          </Button>
+          <a 
+            href="https://www.youtube.com/watch?v=Hi8HzojBkyU&t=2s" 
+            target="_blank" 
+            rel="noopener noreferrer"
+            className="btns btn btn--primary btn--large"
+          >
+            WATCH TRAILER <i className='far fa-play-circle' />
+          </a>
+        </div>
       </div>
     </div>
   );
